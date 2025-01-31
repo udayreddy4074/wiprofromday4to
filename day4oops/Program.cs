fce6d6d3c05875025dcf82097adbe105009bec66
udayreddy4074 <201fa04074@gmail.com>
2025-01-31 08:27:48 +0530

    Add files via upload

--- day4oops/Program.cs
+++ day4oops/Program.cs
@@ -1,49 +1,21 @@
-﻿namespace day4oops
-{ 
+﻿using abstactclass;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("oops");
-         
-            
-            
-
-        
-            Console.WriteLine("eid through base class constructor");
-            int id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("ename through base class constructor");
-            string name = Console.ReadLine();
-            Console.WriteLine("salary through base class constructor");
-             decimal sal = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("dname through base class constructor");
-            string  Dname = Console.ReadLine();
-            Console.WriteLine("mname through base class constructor");
-             string Mname = Console.ReadLine();
-            Man m = new Man(id,name,sal,Dname,Mname );
-
-            m.disp();
-
-            Console.WriteLine(" inheritance by meth");
-            Console.WriteLine("eid");
-            m.eid = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("ename");
-            m.ename = Console.ReadLine();
-            Console.WriteLine("did");
-            m.did = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("salary ");
-            m.sal = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("dname");
-            m.dname = Console.ReadLine();
-            Console.WriteLine("mname");
-            m.mname = Console.ReadLine();
-            m.Eshow();
-            m.Dshow();
-            m.Mshow();
-
-
-            m.disp();
-
-        }
-}
+        Console.WriteLine("abstarct class");
+        //Abstract class cannot be instantiated
+        //It is not mandatory to have abstract method in abstract class.
+        //Generally,we go for abstract in order to show essential features and hide mthd impl
+        // Flight fli = new Flight();
+        //Object Initializer - At the time of object creation,initialize the values
+        fbooking ff = new fbooking() { fno = 1, fname = "kingfisher", timedur = 5, dest = "india" };
+        ff.fdet1();
+        ff.fdet();//instance only by child class
+        Console.WriteLine("instance  by child class and base class");
+        flight f = new fbooking() { fno = 2, fname = "air india", timedur = 12 ,dest="austrailia"};
+        ff.fdet1();
+        ff.fdet();//instance  by child class and base class
     }
+}
---
+++ day4oops/flight.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstactclass
+{
+    abstract class flight
+    {
+        public int fno { get; set; }
+        public string fname { get; set; }
+        public void fdet()//normal method
+        {
+            Console.WriteLine($"flight details {fno}  {fname}");
+        }
+        public void fdet1()
+        {
+        Console.WriteLine("these are normal methods in abstract class");
+        }
+        //public abstract void fdcheck(); it throws error bcz we need to intilize the same method in child class as it mentioned as abstact it  cannot be called when we call the base class through child class
+        public abstract void fdcs();//abstract method declaration
+
+    }
+   class fbooking :flight
+    {
+         public int cost { get; private set; }
+        public int timedur { get; set; }
+        public string dest { get; set; }
+        public override void fdcs()
+        {
+            if (timedur > 10)
+            {
+                cost = 10000;
+            }
+            else
+            {
+                cost = 5000;
+            }
+            if(dest.ToLower().Equals("Inter"))
+                {
+                cost = cost + 2000;
+            }
+            Console.WriteLine($"cost{cost}");
+        }
+    }
+
+}
+
