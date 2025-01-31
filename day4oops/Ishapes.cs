6ba0fa2bf53fdc3362086cbe5d9a8bfe648f7009
udayreddy4074 <201fa04074@gmail.com>
2025-01-31 08:29:16 +0530

    Add files via upload

---
+++ day4oops/Ishapes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfacesdemo
+{
+    internal interface Ishapes
+    {
+        void calarea();//by default interfaces are public and abstract
+        void square(int s)
+        {
+            Console.WriteLine("area of sqaure" + Math.Pow(s, 2));
+        }
+
+    }
+    class circle: Ishapes
+    {
+        public int rad {  get; set; }
+        public void calarea()
+        {
+            Console.WriteLine($"Area of Circle:{Math.PI * Math.Pow(rad, 2)}");
+        }
+    }
+    class rect : circle
+    {
+        public int l { get; set; }
+        public int b { get; set; }
+        public void Calarea()
+        {
+            Console.WriteLine($"Area of Rectangle:{l * b}");
+        }
+    }
+
+}
+
--- day4oops/Program.cs
+++ day4oops/Program.cs
@@ -1,24 +1,36 @@
-﻿
-using gettersetter;
+﻿using interfacesdemo;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("GETTER AND SETTER METHOD");
-        Console.WriteLine("enter id");
-        int id=Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("enter name");
-        string name=Console.ReadLine();
-        Console.WriteLine("enter age");
-        int age=Convert.ToInt32(Console.ReadLine());
-        
-        getsetstu v=new getsetstu();
-        v.ID = id;
-        v.Name = name;
-        v.Age = age;
-        Console.WriteLine(v.ID);
-        Console.WriteLine(v.Name);
-        Console.WriteLine(v.Age);
+        Console.WriteLine("Interface");
+        string u;
+        do
+        {
+            Console.WriteLine("Enter the Shape: 1.Circle 2.Rectangle");
+            string? choice = Console.ReadLine();
+            switch (choice.ToLower())
+            {
+                case "rect":
+                    Console.WriteLine("Enter l and b ");
+                    Ishapes r = new rect() { l = Convert.ToInt32(Console.ReadLine()), b = Convert.ToInt32(Console.ReadLine()) };
+                    r.calarea();
+                    break;
+                case "circle":
+                    Ishapes ish = new circle() { rad = Convert.ToInt32(Console.ReadLine()) };
+                    ish.calarea();
+                    break;
+
+                default:
+                    Console.WriteLine("enter correct choice");
+                    break;
+
+            }
+            Console.WriteLine("do you want to continue(y/n)");
+            u = Console.ReadLine();
+        } while (u.Equals("y"));
+        }
+       
+      
     }
-}
