210c8134c16e9c9e2769af95b8d686cc63d3ea01
udayreddy4074 <201fa04074@gmail.com>
2025-01-31 08:28:48 +0530

    Add files via upload

--- day4oops/Program.cs
+++ day4oops/Program.cs
@@ -1,21 +1,24 @@
-﻿using abstactclass;
+﻿
+using gettersetter;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("abstarct class");
-        //Abstract class cannot be instantiated
-        //It is not mandatory to have abstract method in abstract class.
-        //Generally,we go for abstract in order to show essential features and hide mthd impl
-        // Flight fli = new Flight();
-        //Object Initializer - At the time of object creation,initialize the values
-        fbooking ff = new fbooking() { fno = 1, fname = "kingfisher", timedur = 5, dest = "india" };
-        ff.fdet1();
-        ff.fdet();//instance only by child class
-        Console.WriteLine("instance  by child class and base class");
-        flight f = new fbooking() { fno = 2, fname = "air india", timedur = 12 ,dest="austrailia"};
-        ff.fdet1();
-        ff.fdet();//instance  by child class and base class
+        Console.WriteLine("GETTER AND SETTER METHOD");
+        Console.WriteLine("enter id");
+        int id=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter name");
+        string name=Console.ReadLine();
+        Console.WriteLine("enter age");
+        int age=Convert.ToInt32(Console.ReadLine());
+        
+        getsetstu v=new getsetstu();
+        v.ID = id;
+        v.Name = name;
+        v.Age = age;
+        Console.WriteLine(v.ID);
+        Console.WriteLine(v.Name);
+        Console.WriteLine(v.Age);
     }
 }
---
+++ day4oops/getsetstu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gettersetter
+{
+    internal class getsetstu
+    {
+        private int id;
+        private string name;
+        private int age;
+       // private dname;
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value == "uday"||value== null)
+                {
+                    Console.WriteLine("welcome uday and null is restricted ");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 20 || value == null)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("age must grater than 20");
+                }
+            }
+
+        }
+
+    }
+}
+    
