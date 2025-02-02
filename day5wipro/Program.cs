05fbb5c738caed373133a67245347c6f7aced201
udayreddy4074 <201fa04074@gmail.com>
2025-02-02 09:46:14 +0530

    Add files via upload

--- day5wipro/Program.cs
+++ day5wipro/Program.cs
@@ -1,61 +1,43 @@
-﻿namespace INterfaceconcepts
+﻿using System.Security.Principal;
+using day5files;
+using Newtonsoft.Json;
+
+internal class Program
 {
-    using System;
-    using global::WiproDay5;
-
-    namespace WiproDay5
+    private static void Main(string[] args)
     {
-        internal class Program
+        Console.WriteLine("files");
+        //FileStream fs = new FileStream("filedemo.txt",FileMode.OpenOrCreate );
+        /*StreamWriter sw= new StreamWriter(fs);
+        sw.WriteLine("THIS DEMO FILE BY OPEN OR CREATE(OpenOrCreate) MODE");
+        sw.WriteLine("second line ");
+        Console.WriteLine("file created sucessfully");
+        sw.Close();
+        fs.Close();*/
+        //StreamReader sr = new StreamReader(fs);
+        //Console.WriteLine(sr.ReadLine());
+        // Console.WriteLine(sr.ReadLine());
+        //Console.WriteLine(sr.ReadToEnd());
+        /*string res = "";
+        while(( res=sr.ReadLine() )!= null)
         {
-            private static void Main(string[] args)
-            {
-
-
-                static void Main(string[] args)
-                {
-                   
-
-                        Console.WriteLine("Enter the type of employee 1.Permanent 2.Temporary");
-                        string? type = Console.ReadLine();
-                  
-                    
-                       if (type.ToLower().Contains("per"))
-                        {
-                            //  IEmployee employee = new PermanentEmployee() { empid = 111, empname = "Riya", salary = 70000 };
-                            PermanentEmployee employee = new PermanentEmployee() { empid = 111, empname = "Riya", salary = 70000 };
-                            employee.BasicDetails();
-                            employee.SalaryDetails();
-                            employee.AttendMeetings();
-                            IEmployee emp = new PermanentEmployee() { empid = 113, empname = "Liya", salary = 80000 };
-                            emp.BasicDetails();
-                            emp.SalaryDetails();
-
-                        }
-                        else if (type.ToLower().Contains("temp"))
-                        {
-                            IEmployee tmp = new TemporaryEmployee()
-
-
-                            {
-                                empid = 112,
-                                empname = "Riyaz",
-                                salaryperday = 7000
-                            };
-                            tmp.BasicDetails();
-                            tmp.SalaryDetails();
-                        }
-
-                    }
-                }
-
-            }
+            Console.WriteLine(res);
         }
+        account acc=new account();*/
+        Account acc = new Account() { Accid = 1, AccName = "Zara", AccBalance = 50000, };
+        Account acc1 = new Account() { Accid = 2, AccName = "uday", AccBalance = 10000, };
+        string data = JsonConvert.SerializeObject(acc);
+        Console.WriteLine(acc);
+        FileStream fs = new FileStream("Sample.txt", FileMode.OpenOrCreate);
+        StreamWriter stw = new StreamWriter(fs);
+        stw.WriteLine(acc.Accid+" "+acc.AccName+" "+acc.AccBalance);
+        stw.WriteLine(acc1.Accid + " " + acc1.AccName + " " + acc1.AccBalance);
+        Console.WriteLine("File created successfully");
+        StreamReader sr = new StreamReader(fs);
+        Console.WriteLine(sr.ReadToEnd());
+
+        stw.Close();
+        fs.Close();
+       
     }
-
-                   
-
-            
-    
-
-
-
+}
