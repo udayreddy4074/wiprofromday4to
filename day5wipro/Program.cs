aa3091733254a6412b8e49ccd013b7b14704b9aa
udayreddy4074 <201fa04074@gmail.com>
2025-02-02 09:46:45 +0530

    Add files via upload

--- day5wipro/Program.cs
+++ day5wipro/Program.cs
@@ -1,43 +1,67 @@
-﻿using System.Security.Principal;
-using day5files;
-using Newtonsoft.Json;
-
-internal class Program
+﻿
+using System.Collections;
+ArrayList arrlst = new ArrayList();
+arrlst.Add(111);//Boxing
+arrlst.Add("Wipro");
+arrlst.Add(true);
+Console.WriteLine(arrlst);
+foreach (object i in arrlst)
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("files");
-        //FileStream fs = new FileStream("filedemo.txt",FileMode.OpenOrCreate );
-        /*StreamWriter sw= new StreamWriter(fs);
-        sw.WriteLine("THIS DEMO FILE BY OPEN OR CREATE(OpenOrCreate) MODE");
-        sw.WriteLine("second line ");
-        Console.WriteLine("file created sucessfully");
-        sw.Close();
-        fs.Close();*/
-        //StreamReader sr = new StreamReader(fs);
-        //Console.WriteLine(sr.ReadLine());
-        // Console.WriteLine(sr.ReadLine());
-        //Console.WriteLine(sr.ReadToEnd());
-        /*string res = "";
-        while(( res=sr.ReadLine() )!= null)
-        {
-            Console.WriteLine(res);
-        }
-        account acc=new account();*/
-        Account acc = new Account() { Accid = 1, AccName = "Zara", AccBalance = 50000, };
-        Account acc1 = new Account() { Accid = 2, AccName = "uday", AccBalance = 10000, };
-        string data = JsonConvert.SerializeObject(acc);
-        Console.WriteLine(acc);
-        FileStream fs = new FileStream("Sample.txt", FileMode.OpenOrCreate);
-        StreamWriter stw = new StreamWriter(fs);
-        stw.WriteLine(acc.Accid+" "+acc.AccName+" "+acc.AccBalance);
-        stw.WriteLine(acc1.Accid + " " + acc1.AccName + " " + acc1.AccBalance);
-        Console.WriteLine("File created successfully");
-        StreamReader sr = new StreamReader(fs);
-        Console.WriteLine(sr.ReadToEnd());
-
-        stw.Close();
-        fs.Close();
-       
-    }
+    Console.WriteLine(i);
+}
+Console.WriteLine("Capacity of arlst:" + arrlst.Capacity);
+Console.WriteLine("Number of items in ArrayList:" + arrlst.Count);
+Console.WriteLine();
+string[] cities = { "Chennai", "Bangalore", "Hyderabad", "Bihar" };
+arrlst.AddRange(cities);
+foreach (object i in arrlst)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+Console.WriteLine("Capacity of arlst:" + arrlst.Capacity);
+Console.WriteLine("Number of items in ArrayList:" + arrlst.Count);
+arrlst.Insert(1, 1000);
+foreach (object i in arrlst)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+int[] ar = { 10, 20, 30 };
+arrlst.InsertRange(3, ar);
+foreach (object i in arrlst)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Capacity of arlst:" + arrlst.Capacity);
+Console.WriteLine("Number of items in ArrayList:" + arrlst.Count);
+Console.WriteLine();
+arrlst.Remove("Chennai");
+foreach (object i in arrlst)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+arrlst.RemoveAt(2);
+foreach (object i in arrlst)
+{
+    Console.WriteLine(i);
+}
+arrlst.RemoveRange(1, 3);
+foreach (object i in arrlst)
+{
+    Console.WriteLine(i);
+}
+arrlst[1] = "Update";
+foreach (object i in arrlst)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine(arrlst.Contains("bangalore"));
+ArrayList intarr = new ArrayList();
+intarr.Add(111);
+intarr.Add(112);//Boxing - value type(int) converted to ref type(object)
+foreach (int item in intarr)
+{
+    Console.WriteLine(item);//Unboxing 
 }
