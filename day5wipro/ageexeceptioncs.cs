322bc561c75f1028c0b9e0de3949fd0ed2e387cb
udayreddy4074 <201fa04074@gmail.com>
2025-02-02 09:54:44 +0530

    Add files via upload

---
+++ day5wipro/ageexeceptioncs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day5exceptions
+{
+    internal class ageexeceptioncs: Exception
+
+    {
+        public ageexeceptioncs(string errormsg):base(errormsg) { }
+    }
+}
---
+++ day5wipro/execptionProgram.cs
@@ -0,0 +1,58 @@
+﻿using day5exceptions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Exception handling");
+        try
+        {
+
+            Console.WriteLine("Enter the age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+            {
+                throw new ageexeceptioncs("Invalid Age.You are not eligible to vote");
+            }
+            try
+            {
+                Console.WriteLine("enter value of a");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter value of  b");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("divison of a and b is" + (a / b));
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine(er.Message);
+                Console.WriteLine(er.StackTrace);
+                //Console.WriteLine(er.ToString());
+
+            }
+                
+        }
+        
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (ageexeceptioncs ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+        finally
+        {
+            Console.WriteLine("finall keyword always executed");
+        }
+    }
+
+}
